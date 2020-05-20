bd4757d12186e74de7bb071f3a733ccbe1f68865
AlenaProvotorova <lunichkina.alena@gmail.com>
2020-05-20 17:32:29 +0300

    Add adaptive design and js in faq block

--- app/js/main.js
+++ app/js/main.js
@@ -13,3 +13,33 @@ window.onscroll = function () {
 upButton.onclick = function () {
   window.scrollTo(0, 0);
 };
+
+// faq block
+
+const questions = document.querySelectorAll(".faq-block__question");
+const arrowBtns = document.querySelectorAll(".fa-chevron-right");
+const answers = document.querySelectorAll(".answer");
+const answerTitles = document.querySelectorAll(".answer-title");
+
+function returnArrow() {
+  arrowBtns.forEach((element) => {
+    element.classList.remove("fa-chevron-right--active");
+  });
+}
+
+questions.forEach((element, i) => {
+  element.addEventListener("click", function () {
+    arrowBtns.forEach((elem) =>
+      elem.classList.remove("fa-chevron-right--active")
+    );
+    arrowBtns[i].classList.add("fa-chevron-right--active");
+
+    answers.forEach((elem) => elem.classList.remove("answer--open"));
+    answers[i].classList.add("answer--open");
+
+    answerTitles.forEach((elem) =>
+      elem.classList.remove("answer-title--active")
+    );
+    answerTitles[i].classList.add("answer-title--active");
+  });
+});
